b67ca21394626fb36afb59e1c7bb19b80831a3db
mehaksaini2811 <65498034+mehaksaini2811@users.noreply.github.com>
2020-05-18 10:47:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Forecast App</h1>
+      <main />
+      <footer>Created by Mehak Saini</footer>
+    </div>
+  );
+}
